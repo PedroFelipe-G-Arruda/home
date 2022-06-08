c671f8d7257df8d47c807d6960e8754e58c1ff78
Pedro Felipe Arruda <pedrofelipega@gmail.com>
2022-06-08 12:03:35 -0400

    Updates

---
+++ precache-manifest.2beb87ee108e5557e65ed558cdb5de4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3399ef4c3975cfabe4de3ff4a1e0a2",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "dd84db469f6d0ebe4c7d",
+    "url": "/home/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "091218b5e7b3d235616a",
+    "url": "/home/static/css/main.307a871c.chunk.css"
+  },
+  {
+    "revision": "dd84db469f6d0ebe4c7d",
+    "url": "/home/static/js/2.ea677111.chunk.js"
+  },
+  {
+    "revision": "b71cb3cccc73e099d412c24a04bf9105",
+    "url": "/home/static/js/2.ea677111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "091218b5e7b3d235616a",
+    "url": "/home/static/js/main.1fe5a549.chunk.js"
+  },
+  {
+    "revision": "0a245db243eed24076b4",
+    "url": "/home/static/js/runtime-main.72866795.js"
+  },
+  {
+    "revision": "ee1f8fbf25a13594d0b53b9d3412b5a5",
+    "url": "/home/static/media/resume.ee1f8fbf.pdf"
+  }
+]);
